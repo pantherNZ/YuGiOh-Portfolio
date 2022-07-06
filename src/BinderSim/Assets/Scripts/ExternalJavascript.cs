7357594437c73fe3e5dadeb814a0346e373f7b81
Alex Denford <pantherNZ@users.noreply.github.com>
2022-07-06 01:43:25 -0700

    Import button & javascript

---
+++ src/BinderSim/Assets/Scripts/ExternalJavascript.cs
@@ -0,0 +1,8 @@
+﻿using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class Javascript : MonoBehaviour
+{
+    [DllImport( "__Internal" )]
+    public static extern string BrowserTextUpload( string extFilter, string gameObjName, string dataSinkFn );
+}
