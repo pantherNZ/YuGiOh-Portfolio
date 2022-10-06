3e56d6a9970ce8f3d013cf4da47518900072a368
Alex Denford <pantherNZ@users.noreply.github.com>
2022-10-06 10:03:34 -0700

    Search list prefab starting changes for new layout, card set drop down and alt art selection
    Will follow into action button improvements too afterwards

---
+++ src/BinderSim/Assets/Scripts/UI/SearchListEntry.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SearchListEntry : MonoBehaviour
+{
+    public Image cardImage;
+    public Button leftCardImageButton;
+    public Button rightCardImageButton;
+    public Image backgroundImage;
+    public TMPro.TextMeshProUGUI titleText;
+    public TMPro.TextMeshProUGUI typeText;
+    public TMPro.TMP_Dropdown setDropdown;
+}
