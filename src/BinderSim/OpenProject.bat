97e944a00f29d63bd3d99c449d1620bd6c8088ed
Alex Denford <pantherNZ@users.noreply.github.com>
2022-12-02 01:42:11 -0800

    Materials & start of card shader

--- src/BinderSim/OpenProject.bat
+++ src/BinderSim/OpenProject.bat
@@ -1,2 +1,2 @@
 start "D:\Software\Visual Studio\IDE\Common7\IDE\devenv.exe" "BinderSim.sln"
-start "" "Assets/Resources/Scenes/App.unity"
+start "" "D:\Software\Unity\2021.1.17f1\Editor\Unity.exe" -projectPath "Assets/Resources/Scenes/App.unity"
